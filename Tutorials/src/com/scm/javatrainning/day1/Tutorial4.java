9aac8b1cad05046ac708ea4a413b7eab5a49fd51
Acer <Acer@J-9ine>
2022-02-21 09:46:22 +0630

    Tutorial 4

---
+++ Tutorials/src/com/scm/javatrainning/day1/Tutorial4.java
@@ -0,0 +1,53 @@
+package com.scm.javatrainning.day1;
+
+/**
+ * <h2>Tutorial4 Class</h2>
+ * <p>
+ * Process for Displaying Tutorial4. -To display the pattern like right angle
+ * triangle.
+ * </p>
+ * 
+ * @author Acer
+ *
+ */
+public class Tutorial4 {
+
+	/**
+	 * <h2>displayEvenPattern</h2>
+	 * <p>
+	 * To display the pattern like right angle triangle.
+	 * </p>
+	 *
+	 * @param num - integer
+	 * @return void
+	 */
+	public static void displayEvenPattern(int num) {
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				int x = j + 1;
+				if (x % 2 == 0 || x == 1) {
+					System.out.print(x);
+					if (x != 1) {
+						System.out.print(" ");
+					}
+				}
+
+			}
+			System.out.println();
+		}
+	}
+
+	/**
+	 * <h2>main</h2>
+	 * <p>
+	 * Main method to run project.
+	 * </p>
+	 *
+	 * @param args
+	 * @return void
+	 */
+	public static void main(String[] args) {
+		displayEvenPattern(10);
+	}
+
+}
