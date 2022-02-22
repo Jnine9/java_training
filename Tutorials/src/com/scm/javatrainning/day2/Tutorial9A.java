6a4433b0e90488181f9224fa3aeadeb034b74009
Acer <Acer@J-9ine>
2022-02-22 17:01:58 +0630

    [main] Update Tutorial 9 (A)

--- Tutorials/src/com/scm/javatrainning/day2/Tutorial9A.java
+++ Tutorials/src/com/scm/javatrainning/day2/Tutorial9A.java
@@ -1,9 +1,10 @@
 package com.scm.javatrainning.day2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * <h2> Tutorial9A Class</h2>
+ * <h2>Tutorial9A Class</h2>
  * <p>
  * Process for Displaying Tutorial9A
  * </p>
@@ -13,8 +14,9 @@ import java.util.Scanner;
  */
 public class Tutorial9A {
 	static Scanner input;
+
 	/**
-	 * <h2> testData</h2>
+	 * <h2>testData</h2>
 	 * <p>
 	 * Determine whether number is positive of negative.
 	 * </p>
@@ -25,12 +27,12 @@ public class Tutorial9A {
 	 */
 	public static String testData(int num) {
 		String res = "";
-		res = num > 0 ? "Number is poitive.":
-			"Number is negative.";
+		res = num > 0 ? "Number is poitive." : "Number is negative.";
 		return res;
 	}
+
 	/**
-	 * <h2> main</h2>
+	 * <h2>main</h2>
 	 * <p>
 	 * Main method to run project.
 	 * </p>
@@ -40,9 +42,25 @@ public class Tutorial9A {
 	 */
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
-		System.out.print("Test Data\nEnter Input Number : ");
-		int num = input.nextInt();
-		System.out.println(testData(num));
+		int num = 0;
+		boolean stop = false;
+		String userconfirm = "";
+		System.out.println("Test Data");
+		do {
+			try {
+				System.out.print("Enter Input Number : ");
+				num = input.nextInt();
+				System.out.println(testData(num));
+				System.out.println("Do you want to repeat repeat again ? (y/n)");
+				userconfirm = input.next();
+				stop = userconfirm.equalsIgnoreCase("y") ? false : true;
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter a valid input.");
+				stop = true;
+				break;
+			}
+		} while (!stop);
+
 	}
 
 }
